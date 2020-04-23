3c64455c073809e2f42bfcc4f77ebc8600dc2674
Thibault <dudouit.thibault@outlook.com>
2020-04-23 14:36:19 +0200

    ApplicationContext

---
+++ LEMBack/src/main/java/application/ApplicationContext.java
@@ -0,0 +1,26 @@
+package application;
+
+
+public class ApplicationContext {
+		/*private static IDAOOffre daoOffre;
+		private static IDAOCompte daoCompte;
+		
+		public static IDAOOffre getDaoOffre()
+		{
+			if (daoOffre == null)
+			{
+				daoOffre = new DAOOffreJpa();
+			}
+			return daoOffre;
+		}
+		
+		
+		public static IDAOCompte getDaoCompte()
+		{
+			if (daoCompte == null)
+			{
+				daoCompte = new DAOCompteJpa();
+			}
+			return daoCompte;
+		}*/
+}
