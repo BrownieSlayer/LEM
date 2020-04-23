c47d4339b51c23a5fafcf8369ec4a97f6c373c16
Thibault <dudouit.thibault@outlook.com>
2020-04-23 15:51:21 +0200

    DAOOffreJPA

--- LEMBack/src/main/java/dao/jpa/DAOOffreJPA.java
+++ LEMBack/src/main/java/dao/jpa/DAOOffreJPA.java
@@ -13,50 +13,91 @@ import model.Joueur;
 import model.Manager;
 import model.Offre;
 
-public class DAOOffreJPA implements IDAOOffre {
+public class DAOOffreJPA extends DAOJPA implements IDAOOffre {
 
 	@Override
 	public void insert(Offre t) {
-		// TODO Auto-generated method stub
-		
+		this.em.getTransaction().begin();
+		try {
+		this.em.persist(t);
+		this.em.getTransaction().commit();
+		}
+		catch(Exception e) {
+			this.em.getTransaction().rollback();
+		}	
 	}
 
 	@Override
 	public Offre selectById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.em
+				.createQuery("select o from Offre o where o.id = ?1", Offre.class)
+				.setParameter(1,id)
+				.getSingleResult();
 	}
 
 	@Override
 	public List<Offre> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.em
+				.createQuery("select o from Offre o", Offre.class)
+				.getResultList();
 	}
 
 	@Override
 	public void update(Offre t) {
-		// TODO Auto-generated method stub
-		
+		this.em.getTransaction().begin();
+		try {
+		this.em.merge(t);
+		this.em.getTransaction().commit();
+		}
+		catch(Exception e) {
+			this.em.getTransaction().rollback();
+		}
 	}
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
-		
+		try {
+			this.em.getTransaction().begin();
+			Offre offreToRemove = new Offre();	
+			offreToRemove.setId(id);	
+			this.em.remove(this.em.merge(offreToRemove));
+			this.em.getTransaction().commit();
+			}catch(Exception e) {this.em.getTransaction().rollback();}
 	}
 
-	@Override
+	@Override //Laisser vide
 	public Offre selectOffreByIds(Integer id_joueur, Integer id_manager) {
-		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public Offre selectOffreByComptes(Joueur joueur, Manager manager) {	
+		return this.em
+				.createQuery("select o from Offre where o.joueur = ?1 && o.manager = manager", Offre.class)
+				.setParameter(1,joueur)
+				.setParameter(2,manager)
+				.getSingleResult();
 	}
 
 	@Override
 	public List<Offre> selectAll(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.em
+				.createQuery("select o from Offre o", Offre.class)
+				.getResultList();
 	}
 
+	public void delete(Joueur joueur, Manager manager) {
+		try {
+			this.em.getTransaction().begin();
+			Offre offreToRemove = new Offre();	
+			offreToRemove.setJoueur(joueur);
+			offreToRemove.setManager(manager);
+			this.em.remove(this.em.merge(offreToRemove));
+			this.em.getTransaction().commit();
+			}catch(Exception e) {this.em.getTransaction().rollback();}
+		
+	}
+
+	//Laisser vide
 	@Override
 	public void delete(Integer id_joueur, Integer id_manager) {
 		// TODO Auto-generated method stub
