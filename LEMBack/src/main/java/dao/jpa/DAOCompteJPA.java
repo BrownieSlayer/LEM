343f4d2248e4074ab803ca7653d601617fa35f61
Kodo <kodo.brevet@live.fr>
2020-04-23 16:35:55 +0200

    DAOCompte K
    DAOCompte insert selectby id/pseudo/login/all

--- LEMBack/src/main/java/dao/jpa/DAOCompteJPA.java
+++ LEMBack/src/main/java/dao/jpa/DAOCompteJPA.java
@@ -13,24 +13,31 @@ import model.Joueur;
 import model.Manager;
 import model.Roles;
 
-public class DAOCompteJPA implements IDAOCompte {
+public class DAOCompteJPA extends DAOJPA implements IDAOCompte {
 
 	@Override
-	public void insert(Compte t) {
-		// TODO Auto-generated method stub
+	public void insert(Compte Entity) {
+		this.em.getTransaction().begin();
+		try {
+			this.em.persist(Entity);
+			this.em.getTransaction().commit();
 		
+		}
+		catch(Exception e) {
+			this.em.getTransaction().commit();
+		}
+			
 	}
 
 	@Override
 	public Compte selectById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.em.find(Compte.class, id);
 	}
 
 	@Override
 	public List<Compte> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery( "select c from Compte c", Compte.class).getResultList();
 	}
 
 	@Override
@@ -65,14 +72,13 @@ public class DAOCompteJPA implements IDAOCompte {
 
 	@Override
 	public Compte selectByPseudo(String pseudo) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.em.find(Compte.class, pseudo);
+		
 	}
 
 	@Override
 	public Compte selectByLogin(String login) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.em.find(Compte.class, login);
 	}
 
 }
