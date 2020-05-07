494fb5b7d7b50f36d4b6aabbee9cfba12058b2f4
Florian Massol <florian.massol.pro@gmail.com>
2020-05-07 17:31:37 +0200

    joueurController + html

--- LEMFront/src/main/java/controller/JoueurController.java
+++ LEMFront/src/main/java/controller/JoueurController.java
@@ -1,19 +1,24 @@
 package controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import dao.IDAOCandidature;
 import dao.IDAOCompte;
 import dao.IDAOOffre;
-import model.Compte;
+import model.Candidature;
 import model.Joueur;
+import model.Manager;
+import model.Offre;
 
 
 
@@ -26,6 +31,7 @@ public class JoueurController {
 	@Autowired
 	private IDAOCandidature daoCandidature;
 	
+	
 	@GetMapping("/joueur")
 	public String findAll(Model model, HttpSession session) {
 		Joueur compte=((Joueur)session.getAttribute("compte"));
@@ -37,6 +43,119 @@ public class JoueurController {
 	}
 	
 	
+	@PostMapping("/joueur/updateSalmin")
+	public String updateSalmin(
+			@RequestParam double salmin,
+			@RequestParam ("id_page") int idPage) {
+		
+		Joueur j = (Joueur) daoCompte.findById(idPage).get();
+		j.setSalmin(salmin);
+		daoCompte.save(j);
+		
+		return "redirect:/joueur";
+	}
+	
+	@PostMapping("/joueur/updateRole")
+	public String updateRole(
+			@RequestParam String role,
+			@RequestParam ("id_page") int idPage) {
+		
+		Joueur j = (Joueur) daoCompte.findById(idPage).get();
+		j.setRole(role);
+		daoCompte.save(j);
+		
+		return "redirect:/joueur";
+	}
+	
+	
+	@PostMapping("/joueur/updateStats")
+	public String updateStats(
+			@RequestParam ("id_page") int idPage,
+			@RequestParam double elimination,
+			@RequestParam double mort,
+			@RequestParam double assist, 
+			@RequestParam double kda) {
+	Joueur j = (Joueur) daoCompte.findById(idPage).get();
+	j.setElimination(elimination);
+	j.setMort(mort);
+	j.setAssist(assist);
+	j.setKda(kda);
+	daoCompte.save(j);
+	
+	return "redirect:/joueur";
+	}
+	
+	
+	
+	@PostMapping("/joueur/delete")
+	public String delete(
+			@RequestParam ("id_offre") int idOffre) {
+		
+		daoOffre.deleteById(idOffre);
+		
+		return "redirect:/joueur";
+	}
+	
+	@PostMapping("/joueur/accepter")
+	public String accepter(
+			@RequestParam("id_page") int idPage,
+			@RequestParam("id_offre") int idOffre,
+			@RequestParam("id_joueur") int idJoueur,
+			@RequestParam("id_manager") int idManager,
+			@RequestParam String equipe,
+			@RequestParam String role) {
+		
+		Manager manager = (Manager) daoCompte.selectById(idManager);
+		
+		System.out.println(idJoueur);
+		System.out.println(idManager);
+		System.out.println(equipe);
+		System.out.println(role);
+		
+		Joueur j = (Joueur) daoCompte.findById(idPage).get();
+		j.setEquipe(equipe);
+		j.setRole(role);
+		j.setManager(manager);
+		daoCompte.save(j);
+		daoOffre.deleteById(idOffre);
+		
+		return "redirect:/joueur";
+	}
+	
+	@PostMapping("/joueur/candidater")
+	public String candidater(
+			@RequestParam ("id_joueur") int idJoueur,
+			@RequestParam ("pseudo_manager") String pseudoManager,
+			@RequestParam ("role_demande") String roleDemande,
+			@RequestParam("salaire_demande") double salaireDemande,
+			HttpSession session) {
+		
+		System.out.println("candidature");
+
+		Manager manager = (Manager) daoCompte.selectByPseudo(pseudoManager);
+		System.out.println(manager.getPseudo());
+		if (manager != null)
+		{
+			Joueur joueur = (Joueur) daoCompte.selectById(idJoueur);
+			System.out.println(joueur);
+			System.out.println(roleDemande);
+			String equipeDemande = manager.getEquipe();
+			System.out.println(equipeDemande);
+			System.out.println(salaireDemande);
+			Candidature candidature = new Candidature();
+			System.out.println(candidature);
+			candidature.setJoueur(joueur);
+			candidature.setManager(manager);
+			candidature.setSalaireDemande(salaireDemande);
+			candidature.setEquipeDemande(equipeDemande);
+			candidature.setRoleDemande(roleDemande);
+			daoCandidature.save(candidature);
+			session.setAttribute("candReussi", 1);
+		}
+		session.setAttribute("candReussi", 0);
+		
+		return "redirect:/joueur";
+	}
 	
 //	
 //	
